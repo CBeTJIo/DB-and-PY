f3a9b02b7920502053775569bf918c4f9989903d
Dmitriy_K <dimon_sdk@mail.ru>
2024-02-12 12:10:36 +0300

    Changed DB_Py

--- DB_Py.py
+++ DB_Py.py
@@ -1,5 +1,6 @@
 import psycopg2
 
+
 # Функция создания таблиц
 def create_db(conn):
     cursor.execute("""
@@ -18,9 +19,10 @@ def create_db(conn):
     """)
     conn.commit()
 
+
 # Функция добавления клиентов
 def add_client(conn, first_name, last_name, email, phones=None):
-    cursor.execute(f"""
+    cursor.execute("""
         INSERT INTO users (first_name, last_name, email, phones)
         VALUES (%s, %s, %s,%s) RETURNING id;
         """, (first_name, last_name, email, phones))
@@ -28,25 +30,44 @@ def add_client(conn, first_name, last_name, email, phones=None):
     add_phone(conn, client_id, phones)
     conn.commit()
 
-# Функция добавления телефонов для клиентов
+
 def add_phone(conn, client_id, phones):
-    cursor.execute(f"""
-    INSERT INTO phones (phone_number, client_id)
-    VALUES('{phones}', '{client_id}');
-    """)
-    conn.commit()
+    if phones == None:
+        pass
+    else:
+        cursor.execute("""
+        INSERT INTO phones (phone_number, client_id)
+        VALUES (%s, %s);
+        """, (phones, client_id))
+        conn.commit()
 
-# Функция изменения данных по клиенту
+
 def change_client(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
-    cursor.execute("""
-        UPDATE users
-        SET first_name = %s, last_name = %s, email = %s, phones = %s
-        WHERE id = %s
-    """, (first_name, last_name, email, phones, client_id))
+    if first_name != None:
+        cursor.execute("""
+            UPDATE users
+            SET first_name = %s
+            WHERE id = %s
+        """, (first_name, client_id))
 
-    if phones not in list_phone(conn):
+    elif last_name != None:
+        cursor.execute("""
+            UPDATE users
+            SET last_name = %s
+                WHERE id = %s
+        """, (last_name, client_id))
+
+    elif email != None:
+        cursor.execute("""
+            UPDATE users
+            SET email = %s
+                WHERE id = %s
+        """, (email, client_id))
+
+    if phones not in list_phone(conn) and phones != None:
         add_phone(conn, client_id, phones)
     conn.commit()
+
 
 # Функция удаления телефона клиента
 def delete_phone(conn, client_id, phone):
@@ -55,6 +76,7 @@ def delete_phone(conn, client_id, phone):
         WHERE phone_number = %s AND client_id = %s;
     """, (phone, client_id))
     conn.commit()
+
 
 # Функция удаления клиента
 def delete_client(conn, client_id):
@@ -66,32 +88,26 @@ def delete_client(conn, client_id):
 
 # Функция поиска клиента
 def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
-    if first_name != None or last_name !=None or email != None:
+    if first_name == None or last_name == None or email == None:
+        print(f"Просьба заполнить обязательные поля (first_name, last_name и email)")
+
+    else:
         cursor.execute("SELECT * FROM users")
         users = cursor.fetchall()
         x = 0
         for user in users:
             x += 1
-            if first_name == user[1] or last_name == user[2] or email == user[3]:
-                print(f'Информация о клиенте: id - {user[0]}, имя - {user[1]}, фамилия - {user[2]}, email - {user[3]}.')
+            if first_name == user[1] and last_name == user[2] and email == user[3]:
+                cursor.execute(f"SELECT * FROM phones WHERE client_id = {user[0]}")
+                list_phones = []
+                phones = cursor.fetchall()
+                for search_phone in phones:
+                    list_phones.append(search_phone[1])
+                print(f'Информация о клиенте: id - {user[0]}, имя - {user[1]}, фамилия - {user[2]}, email - {user[3]}, телефон(ы) - {list_phones}.')
                 break
             elif x == len(users):
-                print(f"Клиента {first_name} {last_name} нет в базе") #ДОПИСАТЬ!!!!!
+                print(f"Клиента {first_name} {last_name} нет в базе")
 
-    else:
-        if phone in list_phone(conn):
-            cursor.execute("SELECT * FROM phones")
-            phones = cursor.fetchall()
-            for search_phone in phones:
-                if phone == search_phone[1]:
-                    id_client = search_phone[0]
-                    cursor.execute("SELECT * FROM users")
-                    users = cursor.fetchall()
-                    for user in users:
-                        if id_client == user[0]:
-                            print(f'Информация о клиенте: id - {user[0]}, имя - {user[1]}, фамилия - {user[2]}, email - {user[3]}.')
-        else:
-            print(f"Клиента с номером телефона {phone} нет в базе")
 
 # Функия для сбора списка всех телефонов в базе
 def list_phone(conn):
@@ -102,6 +118,7 @@ def list_phone(conn):
         all_phones.append(number[1])
     return all_phones
 
+
 # Функция для проверки данных (НЕОБЯЗАТЕЛЬНО)
 def read_query(conn, query):
     cursor.execute(query)
@@ -110,6 +127,8 @@ def read_query(conn, query):
 
 with psycopg2.connect(database="my_work", user="postgres", password="password") as conn:
     cursor = conn.cursor()
+
+if __name__ == "__main__":
     # Удаление таблиц
     cursor.execute("""DROP TABLE phones;""")
     cursor.execute("""DROP TABLE users;""")
@@ -120,10 +139,11 @@ with psycopg2.connect(database="my_work", user="postgres", password="password") 
     add_client(conn, 'Бос', 'Ада', 'mefisto@mail.ru', '666')
     add_phone(conn, 1, '8-999-800-10-10')
     add_phone(conn, 1, '8-999-800-10-11')
-    change_client(conn, 1, 'Генри', 'Кавил', 'genry_kav', '8-55555')
+    change_client(conn, 1, None, None, 'genry_kav', '8-55555')
     delete_phone(conn, 1, '13')
     delete_client(conn, 3)
-    find_client(conn, None, 'Кавил', None, None)
+    find_client(conn, 'Генри', 'Кавил', 'genry_kav', None)
+    find_client(conn, 'Джеймс', None, None, None)
     find_client(conn, None, None, None, '007')
 
     # Проверка данных по пользователям (НЕОБЯЗАТЕЛЬНО)
